443a46ee525b96730e690354ed262f466ce0bb12
RoobnAccessCreative <reuben.dubois.000@accesscreative.ac.uk>
2026-05-07 17:05:53 +0100

    Test: migrating to supabase lib for better performance

--- client/src/lib/portal.ts
+++ client/src/lib/portal.ts
@@ -3,11 +3,14 @@ import type {
   codeProjectProps,
   designCardProps,
   designProjectProps,
-  skillProps,
   topProps,
 } from "@/types";
+import { createClient } from "@supabase/supabase-js";
 
-const server: string = import.meta.env.VITE_SERVER_URL;
+const env = import.meta.env;
+
+const server: string = env.VITE_SERVER_URL;
+const database = createClient(env.VITE_SUPABASE_URL, env.VITE_SUPABASE_ANON_KEY);
 
 async function requestServer(url: string) {
   const controller = new AbortController();
@@ -36,8 +39,30 @@ async function requestServer(url: string) {
   }
 }
 
+function tmout(ac: AbortController) {
+  ac.abort();
+  console.log("aborted");
+}
+
 // Home.tsx
-export const fetchSkills = async (): Promise<skillProps[]> => await requestServer("/get-skills");
+export const fetchSkills = async () => {
+  const ac = new AbortController();
+
+  const req = await database
+    .from("rdmp_technical_skils")
+    .select()
+    .order("id")
+    .abortSignal(ac.signal);
+
+  if (req.data) {
+    clearTimeout(setTimeout(tmout, 10000));
+  }
+
+  setTimeout(tmout, 10000); // 10s timeout
+
+  return req;
+};
+/* async (): Promise<skillProps[]> => await requestServer("/get-skills"); */
 
 export const fetchTopProjects = async (): Promise<topProps[]> =>
   await requestServer("/get-repo/top");
